ed97edb78abae87e5bfebbeecbe5ff9891ca0c4e
srujana-patwari <srujanapatwari@gmail.com>
2021-07-16 00:15:59 +0530

    game model is created with fields of 6

--- masters/models.py
+++ masters/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Game(models.Model):
+    name = models.TextField(max_length=200)
+    image = models.ImageField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    status = models.TextField()
+    no_of_players = models.CharField(max_length=200)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return "{} . {}".format(self.title, self.text)
