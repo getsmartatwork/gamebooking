04487adbe9446a4a770b848ad5856e5697344e68
Nharshimha5 <nharshimha5@gmail.com>
2021-07-17 20:40:23 +0530

    city model created

--- masters/admin.py
+++ masters/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game,City
+ 
 # Register your models
 admin.site.register(Game)
+admin.site.register(City)
--- users/models.py
+++ users/models.py
@@ -1,21 +1,4 @@
 from django.db import models
 
 # Create your models here.
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
 
-class City(models.Model):
-    city = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    state = models.CharField(max_length=200)
-    status=models.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
-    
-
-    def publish(self):
-        self.created_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return "{} {}".format(self.city,self.created_date, self.status)
