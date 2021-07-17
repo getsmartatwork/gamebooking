14b24222dbc2c3b3066fead03a4265a1cb791fe0
srujana-patwari <srujanapatwari@gmail.com>
2021-07-17 11:06:45 +0530

    game model is created with 6 fiels

--- masters/admin.py
+++ masters/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Game
+# Register your models
+admin.site.register(Game)
--- masters/models.py
+++ masters/models.py
@@ -7,15 +7,11 @@ from django.utils import timezone
 
 
 class Game(models.Model):
-    name = models.TextField(max_length=200)
-    image = models.ImageField(blank=True, null=True)
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
     created_date = models.DateTimeField(default=timezone.now)
-    status = models.TextField()
-    no_of_players = models.CharField(max_length=200)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    status = models.BooleanField(default=True)
+    no_of_players = models.IntegerField(default=1)
 
     def __str__(self):
-        return "{} . {}".format(self.title, self.text)
+        return "{}".format(self.name)
