8125ef28e5803af9a9acec4e14fa09ad17569647
bhuvanavijay268 <bhuvanavijay268@gmail.com>
2021-07-19 20:11:30 +0530

    updated team-memebr-model

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import profileModel,TeamMember
+from .models import profileModel,TeamMember,Team
 
 # Register your models here.
 
 admin.site.register(profileModel)
 admin.site.register(TeamMember)
+admin.site.register(Team)
